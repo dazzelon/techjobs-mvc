f6f8b11768d93bad8a8b409f80ec8b3f954f4962
dazzelon <44827215+dazzelon@users.noreply.github.com>
2019-03-18 19:16:48 -0500

    Add files via upload

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -1,5 +1,7 @@
 package org.launchcode.controllers;
 
+import javafx.scene.control.RadioButton;
+import org.launchcode.controllers.ListController;
 import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,6 +24,27 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value = "/results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        model.addAttribute("columns", ListController.columnChoices);
+
+        ArrayList results;
+        int numOfResults = 0;
+
+        if("all".equals(searchType))
+            results = JobData.findByValue(searchTerm);
+        else
+            results = JobData.findByColumnAndValue(searchType,searchTerm);
+
+        if(!results.isEmpty()) {
+            numOfResults = results.size();
+            model.addAttribute("jobs", results);
+        }
+
+        model.addAttribute("number",numOfResults);
+
+        return "search";
+    }
 
 }
